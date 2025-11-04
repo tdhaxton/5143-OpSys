086e3f9af5db60c786a6f5ba9af5513276a1810b
Coop-Wolf <156962773+Coop-Wolf@users.noreply.github.com>
2025-11-04 14:44:57 +0000

    ShortJobFirst

--- Assignments/A02/starter_code/utils/__init__.py
+++ Assignments/A02/starter_code/utils/__init__.py
@@ -1,5 +1,6 @@
 from .clock import Clock
 from .cpu import CPU
 from .iodevice import IODevice
+from .shortestJobFirst import ShortestJobFirst
 
-__all__ = ["Clock", "CPU", "IODevice"]
+__all__ = ["Clock", "CPU", "IODevice", "ShortestJobFirst"]
